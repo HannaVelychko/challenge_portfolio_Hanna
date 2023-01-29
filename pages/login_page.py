a560f405ee287961d7418b238a8e99ca530c9639
HannaVelychko <anna.ua19@gmail.com>
2023-01-29 23:16:50 +0100

    Added first test case

--- pages/login_page.py
+++ pages/login_page.py
@@ -4,7 +4,8 @@ from pages.base_page import BasePage
 class LoginPage(BasePage):
     login_field_xpath = "//*[@id='login']"
     password_field_xpath = "//*[@id='password']"
-    sign_in_button_xpath = "//*[@id='__next']/form/div/div[2]/button/span[1]"
+    sign_in_button_xpath = "//*[text()= 'Sing in']"
+    login_url = ('https://scouts-test.futbolkolektyw.pl/en')
 
     def type_in_email(self, email):
         self.field_send_keys(self.login_field_xpath, email)
